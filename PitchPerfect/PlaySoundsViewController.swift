6ba9843abd0ae499d214aef9dbb5d90c54ed24b6
Obinna <obinna.aguwa@gmail.com>
2022-06-05 04:08:31 +0100

    segue to  the PlaySoundsViewController with audioRecorder url

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Obinna Aguwa on 05/06/2022.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+    var recordedAudioURL: URL!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(recordedAudioURL)
+    }
+    
+    
+}
